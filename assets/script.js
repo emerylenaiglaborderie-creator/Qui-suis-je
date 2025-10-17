66d3c22c9c66b4e65783e58b61f8d94c72d4d638
Emery <emery.laborderie@gmail.com>
2025-10-17 12:22:27 +0200

    first commit

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,21 @@ function toggleContent(element) {
   element.classList.toggle('active');
 }
 
-window.onscroll=()=>{
-    var navbar=document.getElementById("navbar");
-    if( document.documentElement.scrollTop > 30)
-        navbar.style.top="0px";
-    else
-        navbar.style.top="-70px";
-}
+const move =document.getElementById("move");
+let isFollowing = false;
+
+move.addEventListener("click",() => {
+  isFollowing = !isFollowing;
+  move.style.cursor = isFollowing ? "default" : "pointer";
+});
+
+document.body.onpointermove = event => {
+  if (!isFollowing) return;
+
+  const {clientX, clientY} = event;
+
+  move.animate({
+    left:`${clientX - move.width / 2}px`,
+    top: `${clientY - move.height / 2}px`
+  }, { duration: 100, fill: "forwards" });
+};
