9954587ba17135dc4cd28df880d54e0d250a89c3
Emery <emery.laborderie@gmail.com>
2025-10-31 09:21:11 +0100

    fixing bug 1

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,3 @@
 function toggleContent(element) {
   element.classList.toggle('active');
 }
-
-const move =document.getElementById("move");
-let isFollowing = false;
-
-move.addEventListener("click",() => {
-  isFollowing = !isFollowing;
-  move.style.cursor = isFollowing ? "default" : "pointer";
-});
-
-document.body.onpointermove = event => {
-  if (!isFollowing) return;
-
-  const {clientX, clientY} = event;
-
-  move.animate({
-    left:`${clientX - move.width / 2}px`,
-    top: `${clientY - move.height / 2}px`
-  }, { duration: 100, fill: "forwards" });
-};
